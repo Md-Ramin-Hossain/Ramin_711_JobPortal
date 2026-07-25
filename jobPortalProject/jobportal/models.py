5452888b152f25095b09b8aecd919bfef50b74c7
Md. Ramin Hossain <ramink2348@gmail.com>
2026-07-26 00:21:11 +0600

    Create RecruiterProfile

--- jobPortalProject/jobportal/models.py
+++ jobPortalProject/jobportal/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-
+#Create User
 class User(AbstractUser):
     
     USER_TYPES = [
@@ -15,3 +15,26 @@ class User(AbstractUser):
     
     def __str__(self):
       return f'{self.username} - {self.user_type}'
+
+
+
+#create RecuiterProfile
+class RecruiterProfileModel(models.Model):
+    
+    # relation field
+    recruiter = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recruiter_profile',
+        null=True
+    )
+    company_name = models.CharField(max_length=200, null=True)
+    address = models.TextField(null=True)
+    contact = models.CharField(max_length=20, null=True)
+    logo = models.ImageField(upload_to='company_logo', null=True)
+    
+    created_at = models.DateField(auto_now_add=True, null=True)
+    updated_at = models.DateField(auto_now=True, null=True)
+    
+    def __str__(self):
+      return f'{self.company_name}'
