4785257178a0b8adc4ad61410383d85a6faf84ae
Md. Ramin Hossain <ramink2348@gmail.com>
2026-07-26 00:18:52 +0600

    Create User Model

--- jobPortalProject/jobportal/models.py
+++ jobPortalProject/jobportal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+class User(AbstractUser):
+    
+    USER_TYPES = [
+        ('Recruiter', 'Recruiter'),
+        ('Seeker', 'Seeker'),
+    ]
+    
+    display_name = models.CharField(max_length=200, null=True)
+    user_type = models.CharField(choices=USER_TYPES, max_length=20, null=True)
+    
+    def __str__(self):
+      return f'{self.username} - {self.user_type}'
